ce5dcb55ac6e0181d17a1827f9ccb93fec26d459
liuxinmeng <1013430695@qq.com>
2019-09-04 20:18:09 +0800

    add 02-global-var demo code

---
+++ 03-console/01-format.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const log=console.log;
+var wd={
+  'Name':'王顶',
+  'QQ':'408542507',
+  'Age':43
+};
+log('Name:%s',wd.Name);
+log('Age:%d',wd.Age);
+log('WangDing Info',wd);
+log('Name:',wd.Name);
+log('Age is $(wd.Age)');
